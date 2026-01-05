7fe38d89cfa6b27ec82c49e095fd1aa987bee1b6
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2026-01-05 22:08:02 +0200

    feat: better request raw text reading & tokenization of the input text

--- tokenazing-text.py
+++ tokenazing-text.py
@@ -1,16 +1,28 @@
 import os
 import urllib.request
+import requests
+import re
 
-if not os.path.exists('the-verdict.txt'):
+# more robust version of fetching data using request
+# and efficient for VPN user
+if not os.path.exists("the-verdict.txt"):
     url = (
         "https://raw.githubusercontent.com/rasbt/"
         "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
         "the-verdict.txt"
     )
-    filepath = "the-verdict.txt"
-    urllib.request.urlretrieve(url, filepath)
+    file_path = "the-verdict.txt"
+
+    response = requests.get(url, timeout=30)
+    response.raise_for_status()
+    with open(file_path, "wb") as f:
+        f.write(response.content)
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-print(raw_text)
+result = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+# strip whitespace from each item and then filter out any empty strings
+result = [item.strip() for item in result if item.strip()]
+print(result)
+print(len(result))
