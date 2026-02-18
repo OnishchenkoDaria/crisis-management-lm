551aa54522167d18b5aa2c9dda6cfd47aa3c79e1
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2026-02-18 20:18:45 +0200

    feat: adding find one by filter or id, adding endpoint

--- app/users/router.py
+++ app/users/router.py
@@ -11,3 +11,11 @@ router = APIRouter(
 @router.get("/", summary="Get all users")
 async def get_users(request_body: RBUser = Depends()) -> list[SchemaUser]:
     return await UserDAO.find_all(**request_body.to_dict())
+
+
+@router.get("/{id}", summary="Get user by id")
+async def get_user_by_id(user_id: int) -> SchemaUser | dict:
+    result = await UserDAO.find_one_or_none_by_id(user_id)
+    if result is None:
+        return {'message': f'No user with id = {user_id} is found'}
+    return result
