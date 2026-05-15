8541600f571bc2f10423bdad2bc1730664bac830
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2026-05-15 22:30:27 +0300

    fix: short password resolve
    Signed-off-by: OnishchenkoDaria <daria.onishchenko.57@gmail.com>

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -12,7 +12,7 @@ class SchemaUser(BaseModel):
 class SchemaUserAdd(BaseModel):
     name: str = Field(..., min_length=1, max_length=50, description="User name 1-50 symbols")
     email: EmailStr = Field(..., description="User's email")
-    password: str = Field(..., min_length=1, max_length=8, description="User's password")
+    password: str   = Field(..., min_length=8, max_length=128, description="User's password")
 
 class SchemaUserNameUpd(BaseModel):
     name: str = Field(..., min_length=1, max_length=50, description="User name 1-50 symbols")
